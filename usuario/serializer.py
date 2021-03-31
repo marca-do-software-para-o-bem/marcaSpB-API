b6a4a7535eb1140b8ecc957b43bf3a71ff6b1973
CorreiaJV <correia.jv16@gmail.com>
2021-03-31 15:37:58 -0300

    attSerializer

--- usuario/serializer.py
+++ usuario/serializer.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from . import models
+from .models import Account
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username', 'password', 'email']
+        model = Account
+        fields = ['user','cep','endereco','cnpj']
         extra_kwargs = {'password': {'write_only': True}}
     
     def create(self, validated_data):
@@ -15,6 +15,12 @@ class UserSerializer(serializers.ModelSerializer):
         )
         user.set_password(validated_data['password'])
         user.save()
+        account= Account(
+            user = validated_data['user'],
+            cep= validated_data['cep'],
+            endereco= validated_data['endereco'],
+            cnpj= validated_data['cnpj']
+        )
         return user
 
     def update(self, validated_data):
